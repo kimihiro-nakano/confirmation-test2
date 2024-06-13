1eb8eee7a469c7453b013121d329762fa4241b6d
中野公博 <hippo0251@yahoo.co.jp>
2024-06-14 05:30:49 +0900

    a

--- src/app/Http/Controllers/CategoryController.php
+++ src/app/Http/Controllers/CategoryController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Contact;
 use Illuminate\Http\Request;
-use App\Http\Requests\ContactRequest;
-use Illuminate\Pagination\Paginator;
+use App\Http\Requests\SearchRequest;
+
 
 class CategoryController extends Controller
 {
@@ -14,24 +14,73 @@ class CategoryController extends Controller
     {
         $categories = Category::all();
         $contacts = Contact::paginate(7);
-        return view('admin', compact('contacts', 'categories'));
+        $keyword = '';
+        $gender = null;
+        $categoryId = null;
+        $date = null;
+        $exactMatch = false;
+        return view('admin', compact('contacts', 'categories', 'keyword', 'gender', 'categoryId', 'date', 'exactMatch'));
     }
 
-    public function search(ContactRequest $request)
+    public function search(SearchRequest $request)
     {
-        $query = Contact::query();
-        $query->nameEmailSearch($request)->get();
+        $keyword = $request->input('keyword');
+        $exactMatch = $request->input('exact_Match', false);
+        $gender = $request->input('gender', 'all');
+        $categoryId = $request->input('category_id');
+        $date = $request->input('date');
 
-        $contacts = $query->paginate(7);
+        dd($keyword, $gender, $categoryId, $date);
+
+        // $contactsQuery = Contact::when($keyword, function ($query, $keyword) use ($exactMatch) {
+        //     if ($exactMatch) {
+        //         return $query->where(function ($q) use ($keyword) {
+        //             $q->where('first_name', 'like', "%{$keyword}%")
+        //                 ->orWhere('last_name', 'like', "%{$keyword}%")
+        //                 ->orWhere('email', 'like', "%{$keyword}%");
+        //         });
+        //     } else {
+        //         return $query->where(function ($q) use ($keyword) {
+        //             $q->where('first_name', 'like', "%{$keyword}%")
+        //                 ->orWhere('last_name', 'like', "%{$keyword}%")
+        //                 ->orWhere('email', 'like', "%{$keyword}%");
+        //         });
+        //     }
+        // })
+        // ->when($gender !== 'all', function ($query) use ($gender) {
+        //     return $query->where('gender', $gender);
+        // })
+        // ->when($categoryId, function ($query) use ($categoryId) {
+        //     return $query->where('category_id', $categoryId);
+        // })
+        // ->when($date, function ($query) use ($date) {
+        //     return $query->whereDate('created_at', $date);
+        // });
+
+        $contactsQuery = Contact::search(
+            $keyword,
+            $exactMatch,
+            $gender,
+            $categoryId,
+            $date
+        );
+
+        $contacts = $contactsQuery->paginate(7);
+
         $categories = Category::all();
 
-        return view('admin', compact('contacts', 'categories'));
+        return view('admin', compact('contacts', 'categories', 'keyword', 'gender', 'categoryId', 'date', 'exactMatch'));
+    }
+
+    public function reset()
+    {
+        return redirect()->route('admin');
     }
 
     public function destroy(Request $request)
     {
-        Category::find($request->id)->delete();
+        Contact::find($request->id)->delete();
 
-        return redirect('/admin');
+        return redirect('admin');
     }
 }
---
+++ src/app/Http/Controllers/ModelController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ModelController extends Controller
+{
+    public function modal()
+    {
+        return view('modal');
+    }
+}
